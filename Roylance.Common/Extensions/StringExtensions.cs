6c19549db8796b119f7f5944b1d8aa6f6615dcd5
Mike Roylance <roylance.michael@gmail.com>
2014-07-06 13:38:34 -0600

    added in string utility of reading resource

---
+++ Roylance.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+namespace Roylance.Common
+{
+	public static class StringExtensions
+	{
+		public static string ReadResource(this string resourceLocation)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			using (var d1Stream = assembly.GetManifestResourceStream(resourceLocation))
+			{
+				if (d1Stream == null)
+				{
+					return string.Empty;
+				}
+
+				using (var stringReader = new StreamReader(d1Stream))
+				{
+					return stringReader.ReadToEnd();
+				}
+			}
+		}
+	}
+}
+
